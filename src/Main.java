60e05f82756a751e7261e121377e1b5f6fe6de4c
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-02 17:39:48 +0300

    Alt Dize Çıkarma

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
 
 // Büyük ve Küçük harfe dönüşüm: Giriş olarak bir dize alan ve bu diziyi tamamen büyük harf ve tamamen küçük harf olarak görüntüleyen bir program oluşturun.
 
-public class Main{
+/*public class Main{
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
 
@@ -32,4 +32,29 @@ public class Main{
         System.out.println("Büyük harflerle: "+ buyuk);
 
     }
+}*/
+
+// Alt dize çıkarma: Kullanıcıdan bir dize alıp bu dizinin belirli bir alt dizesini çıkararak görüntüleyen bir java programı yazın.
+
+public class Main{
+    public static void main(String[] args){
+        Scanner get= new Scanner(System.in);
+
+        System.out.print("Bir dize giriniz: ");
+        String dize = get.nextLine();
+
+        System.out.print("Başlangıç indeksini girin: ");
+        int start = get.nextInt();
+        System.out.print("Bitiş indeksini girin: ");
+        int end = get.nextInt();
+
+        if(start >= 0 && end <= dize.length() && start <= end){
+            String altDize = dize.substring(start, end);
+            System.out.println("Çıkarılan Alt Dize: "+ altDize);
+        } else{
+            System.out.println("Hata! Geçersiz indeks aralığı!");
+        }
+
+    }
 }
+
