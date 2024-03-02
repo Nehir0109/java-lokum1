0fe64925811c41417c8e9a41ab33ae690da5d093
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-02 17:18:40 +0300

    Büyük ve Küçük harfe dönüşüm

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 //Dize Uzunluğu: Kullanıcı tarafından girilen bir dizenin uzunluğunu bulup görüntülemek için bir Java programı yazın.
-public class Main {
+/*public class Main {
     public static void main(String[] args){
         Scanner get= new Scanner(System.in);
 
-        System.out.print("Bir dize giriniz: ");
+        System.out.print("Bir cümle giriniz: ");
         String dize= get.nextLine();
 
         int uzunluk = dize.length();
 
         System.out.println("Dizenin uzunluğu = "+ uzunluk);
+
+    }
+}*/
+
+// Büyük ve Küçük harfe dönüşüm: Giriş olarak bir dize alan ve bu diziyi tamamen büyük harf ve tamamen küçük harf olarak görüntüleyen bir program oluşturun.
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+
+        System.out.print("Bir cümle giriniz: ");
+        String dize = get.nextLine();
+
+        String kucuk = dize.toLowerCase();
+        String buyuk = dize.toUpperCase();
+
+        System.out.println("Küçük harflerle: "+kucuk);
+        System.out.println("Büyük harflerle: "+ buyuk);
 
     }
 }
